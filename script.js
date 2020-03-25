2bb5c78aad48323b52e9df62fa7df00ab05728ce
Marie Erin <m.oshaughnessy@outlook.com>
2020-03-25 19:50:38 -0400

    added to javascript

--- script.js
+++ script.js
@@ -1,41 +1,24 @@
-const momQuiz = [
-    [1, 2, 3,],
-    [3, 1, 2,],
-    [1, 3, 2],
-]
-
 
 $(function() {
     function formSubmitted() {
         console.log('subbb')
 
         //find out which radio button has been clicked
-        //add together
-        // loop through see which is selected and grab that value then pass it onto the final equation 
-        //Could be a for-loop, but if you're feeling adventurous, there are some specialty array methods we didn't get to in class yet but are super awesome:
-        // https://github.com/HackerYou/bootcamp-notes/blob/master/applied-javascript/advanced-array-methods.md
-        
+        //and add together
+        const question1 = parseInt($('input[name=question1]:checked').val(), 10);
+        const question2 = parseInt($('input[name=question2]:checked').val(), 10);
+        const question3 = parseInt($('input[name=question3]:checked').val(), 10);
         //evaluation section
-        // display the percentage that my mom would like you
-        const question1 = $('input[name=question1]:checked').val();
-        const question2 = $('input[name=question2]:checked').val();
-        const question3 = $('input[name=question3]:checked').val();
-        console.log(question1)
-        console.log(question2)
-        console.log(question3)
+        // my mom doesnt really like anyone less than 44%
+        const x =  Math.floor(question1 + question2 + question3);
+        const finalAnswer =  Math.round(x / 9 * 100);
+        console.log(finalAnswer)
         
-        const x =  Math.floor(question1 + question2);
-        const y = Math.floor(x + question3);
-        const z = Math.floor(y / 9)
-
-        // const x = Math.round(finalAnswer / 9)
-        // console.log(finalAnswer)
-        console.log(x, y, z)
-
-
-        // momQuiz.forEach(momQuiz => {
-        //     console.log(momQuiz)
-        // });
+        // display the percentage that my mom would like you
+        function displayFinalAnswer() {
+            $('.percentageAnswer').html(`<p>% ${finalAnswer}</p>`)
+        }
+        displayFinalAnswer();
     }
 
     // Next button scrolls to the next question
